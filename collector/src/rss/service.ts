d9f36566de7aee0de0700382b14458d0bed3a06b
kei0528 <keisuketanaka97@gmail.com>
2026-01-05 23:14:27 +0100

    feat: implement error handling in getAndStoreNews method for fetching and storing news items

--- collector/src/rss/service.ts
+++ collector/src/rss/service.ts
@@ -1,8 +1,22 @@
+import { log, tryCatch } from 'common';
 import { getNewsRss } from './adapter';
+import { setNewsItems } from './repository';
 
 class RssService {
   static async getAndStoreNews() {
-    await getNewsRss();
+    const [news, getNewsError] = await tryCatch(getNewsRss());
+    if (getNewsError) {
+      log.error(`Error fetching news RSS: ${getNewsError.message}`);
+      return;
+    }
+
+    const [, setNewsError] = await tryCatch(setNewsItems(news));
+    if (setNewsError) {
+      log.error(`Error storing news items: ${setNewsError.message}`);
+      return;
+    }
+
+    log.info('Successfully fetched and stored news items from RSS feed.');
   }
 }
 
